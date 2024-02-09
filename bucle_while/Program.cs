c093bd8d86427d0426c8a841964291ccac917e1d
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-09 09:57:38 -0400

    Agregar archivos de proyecto.

---
+++ bucle_while/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("imprimir en pantalla del 1 al 10");
+Console.WriteLine();
+int i = 1;
+ while(i<=10)
+{
+    Console.WriteLine("numero " + i);
+    i++;
+}
